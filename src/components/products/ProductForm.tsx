dc9636c79577270a9db0c23e0b78c91e725ecb30
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2025-07-29 18:35:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/products/ProductForm.tsx
+++ src/components/products/ProductForm.tsx
@@ -254,14 +254,13 @@ export function ProductForm({ initialData, isSubmitting, onSubmit: onSubmitProp 
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Product Tag</FormLabel>
-                       <Select onValueChange={field.onChange} defaultValue={field.value ?? ""}>
+                       <Select onValueChange={field.onChange} defaultValue={field.value ?? undefined}>
                         <FormControl>
                           <SelectTrigger>
                             <SelectValue placeholder="Select a tag to display on the product" />
                           </SelectTrigger>
                         </FormControl>
                         <SelectContent>
-                          <SelectItem value="">None</SelectItem>
                           {productTags.map(tag => (
                             <SelectItem key={tag} value={tag}>{tag}</SelectItem>
                           ))}
@@ -383,5 +382,3 @@ function VariantField({ form, variantIndex, removeVariant }: { form: any, varian
         </div>
     )
 }
-
-    
