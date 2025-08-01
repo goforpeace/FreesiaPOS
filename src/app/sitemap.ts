c0df5bdc19bf84a1ae68ed9dfe45f000e293bb7e
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2025-08-01 10:49:59 +0000

    how to do SEO to bring this page at top on Google search

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+
+import { getProducts } from '@/lib/api';
+import { MetadataRoute } from 'next';
+
+const URL = 'https://freesia-finds-pos-82fbs.web.app';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await getProducts();
+
+  const productEntries: MetadataRoute.Sitemap = products
+    .filter(product => !product.isRejected && product.quantity > 0)
+    .map(({ id, createdAt }) => ({
+      url: `${URL}/product/${id}`,
+      lastModified: createdAt ? new Date(createdAt) : new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    }));
+
+  return [
+    {
+      url: URL,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+        url: `${URL}/checkout`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+    },
+    ...productEntries,
+  ];
+}
